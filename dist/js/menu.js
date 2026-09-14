661685c46691ae00d3c8d36f04d59a7e1f018107
Ringo <83055225+Ringo0403@users.noreply.github.com>
2026-09-14 17:29:42 +0900

    サイズ変更でハンバーガーメニューを操作後に画面派を広げた際にスクロール出来なくなる現象を修正

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -1,5 +1,11 @@
 // ハンバーガーメニュー
 
+// 2026-09-14 修正: メニューを閉じる共通処理（.openbtnがPC幅でdisplay:noneになった後も呼べるよう関数化）
+function closeNav() {
+	$(".openbtn").removeClass('active');
+	$("#g-nav").removeClass('panelactive');
+}
+
 $(".openbtn").click(function() { //ボタンがクリックされたら
 	$(this).toggleClass('active'); //ボタン自身に activeクラスを付与し
 	$("#g-nav").toggleClass('panelactive'); //ナビゲーションにpanelactiveクラスを付与
@@ -13,24 +19,31 @@ $("#g-nav a").click(function(e) {
 	}
 
 	// 通常リンクなら nav を閉じる
-	$(".openbtn").removeClass('active');
-	$("#g-nav").removeClass('panelactive');
+	// 2026-09-14 修正: 直書きの removeClass 2行を closeNav() 呼び出しに置き換え
+	closeNav();
 });
 
 //ドロップダウンの設定を関数でまとめる
+// 2026-09-14 修正: ブレークポイントを 992px → 1024px に変更（.openbtn の表示切替 _nav.scss の @include lg = 1024px と揃える）
 function mediaQueriesWin() {
 	var width = $(window).width();
-	if (width <= 992) { //横幅が992px以下の場合 $(".has-child>a").off('click');	//has-childクラスがついたaタグのonイベントを複数登録を避ける為offにして一旦初期状態へ
-		$(".has-child>a").on('click', function() { //has-childクラスがついたaタグをクリックしたら
+	if (width <= 1024) { //横幅が1024px以下の場合 $(".has-child>a").off('click');	//has-childクラスがついたaタグのonイベントを複数登録を避ける為offにして一旦初期状態へ
+		// 2026-09-14 修正: リサイズで複数回on登録されないよう off().on() に変更
+		$(".has-child>a").off('click').on('click', function() { //has-childクラスがついたaタグをクリックしたら
 			var parentElem = $(this).parent(); // aタグから見た親要素のliを取得し
 			$(parentElem).toggleClass('active-menu'); //矢印方向を変えるためのクラス名を付与して
 			$(parentElem).children('ul').stop().slideToggle(500); //liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
 			return false; //リンクの無効化
 		});
-	} else { //横幅が992px以上の場合
+	} else { //横幅が1024px超の場合
 		$(".has-child>a").off('click'); //has-childクラスがついたaタグのonイベントをoff(無効)にし
 		$(".has-child").removeClass('active-menu'); //activeクラスを削除
 		$('.has-child').children('ul').css("display", ""); //スライドトグルで動作したdisplayも無効化にする
+
+		// 2026-09-14 修正: PC幅では.openbtnがdisplay:noneになり閉じる手段が無くなるため、
+		// メニューを開いたままリサイズされた場合は強制的に閉じてbodyのスクロールロックを解除する
+		// （スマホ表示で開閉後にPC幅へ広げるとスクロール不可になる不具合の対策）
+		closeNav();
 	}
 }
 
