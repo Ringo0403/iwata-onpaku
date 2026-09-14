58ae209d790c7171b235ae324c0d7031dceb576a
Ringo <83055225+Ringo0403@users.noreply.github.com>
2026-09-14 15:45:57 +0900

    カテゴリのテキスト修正

--- archive-program.php
+++ archive-program.php
@@ -13,16 +13,16 @@
 			<ul class="p-cate-list">
 				<li class="p-cate-list__cate1"><a href="<?php echo home_url();?>/cate_prg/special/">Iwata ON-Paku Special<br><span>いわたおんぱくスペシャル企画</span></a></li>
 				<li class="p-cate-list__cate2"><a href="<?php echo home_url(); ?>/cate_prg/adve/">ちょっとアドベンチャー<br><span>海へ、野へ、はじめての冒険</span></a></li>
-				<li class="p-cate-list__cate3"><a href="<?php echo home_url(); ?>/cate_prg/history/">いわたの歴史を紐解く体験<br><span>古道・伝承・山城・祭り文化</span></a></li>
+				<li class="p-cate-list__cate3"><a href="<?php echo home_url(); ?>/cate_prg/history/">磐田の歴史を紐解く体験<br><span>古道・伝承・山城・祭り文化</span></a></li>
 				<li class="p-cate-list__cate4"><a href="<?php echo home_url(); ?>/cate_prg/wellbe/">ウェルビーイング&セルフケア体験<br><span>心地よい自分に出会う！</span></a></li>
 			</ul>
 		</div>
 		<div>
 			<ul class="p-cate-list">
 				<li class="p-cate-list__cate5"><a href="<?php echo home_url(); ?>/cate_prg/food/">食と農の体験プログラム<br><span>おいしいの、先へ！</span></a></li>
-				<li class="p-cate-list__cate6"><a href="<?php echo home_url(); ?>/cate_prg/sports/">おいしいの、先へ！<br><span>スポーツも、趣味も、出会いも</span></a></li>
-				<li class="p-cate-list__cate7"><a href="<?php echo home_url(); ?>/cate_prg/inspi/">発見と創造の体験<br><span>発見と創造の体験</span></a></li>
-				<li class="p-cate-list__cate8"><a href="<?php echo home_url(); ?>/cate_prg/lifestyle/">暮らし・学びのセミナ<br><span>学びが、暮らしをもっと豊かに</span></a></li>
+				<li class="p-cate-list__cate6"><a href="<?php echo home_url(); ?>/cate_prg/sports/">スポーツ体験＆体験型交流会<br><span>スポーツも、趣味も、出会いも</span></a></li>
+				<li class="p-cate-list__cate7"><a href="<?php echo home_url(); ?>/cate_prg/inspi/">発見と創造の体験<br><span>アート・ものづくり・仕事体験</span></a></li>
+				<li class="p-cate-list__cate8"><a href="<?php echo home_url(); ?>/cate_prg/lifestyle/">暮らし・学びのセミナー<br><span>学びが、暮らしをもっと豊かに</span></a></li>
 			</ul>
 		</div>
 	</div>
